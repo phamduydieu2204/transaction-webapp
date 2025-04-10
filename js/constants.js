9d13edb1242af29b16ed1df95bdb2801765f1bb9
phamduydieu2204 <141470706+phamduydieu2204@users.noreply.github.com>
2025-04-10 17:39:51 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,4 +1,4 @@
-function getAppConstants() {
+function getConstants() {
   return {
     BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxYb5Kppy8wC4a18nUXAy_yCVup1jpdAkmyflLsVWo7VedylZwbf5DY2YwTK74UAHSl/exec'
   };
