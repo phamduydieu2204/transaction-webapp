e62602d0eabb87b1763987b084b6355c5bd9402e
phamduydieu2204 <141470706+phamduydieu2204@users.noreply.github.com>
2025-04-10 16:49:38 +0700

    Update constants.js

---
+++ js/constants.js
@@ -0,0 +1,6 @@
+function getConstants() {
+    return {
+      BACKEND_URL: 'https://script.google.com/macros/s/AKfycbxYb5Kppy8wC4a18nUXAy_yCVup1jpdAkmyflLsVWo7VedylZwbf5DY2YwTK74UAHSl/exec'
+    };
+  }
+  
