bb0f54745b66a0e719de0831f13f6d2e48b7e784
phamduydieu2204 <141470706+phamduydieu2204@users.noreply.github.com>
2025-04-12 17:45:13 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 function getConstants() {
   return {
-    BACKEND_URL: 'https://cors-anywhere.herokuapp.com/https://script.google.com/macros/s/AKfycbz1ESh_ksiFP5jde9GQAqZL2_rCssmENUFzUrjNSWfzzrypESB63XT2Cb0rcPOFxdbN/exec'
+    BACKEND_URL: 'http://localhost:3000/api/proxy'
   };
 }
