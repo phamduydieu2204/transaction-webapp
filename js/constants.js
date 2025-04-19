f408dabc231e901c9a8ca84d15c434d9d8aed359
phamduydieu2204 <141470706+phamduydieu2204@users.noreply.github.com>
2025-04-19 16:02:09 +0700

    Update constants.js

--- js/constants.js
+++ js/constants.js
@@ -1,5 +1,5 @@
 function getConstants() {
   return {
-    BACKEND_URL: 'https://sleepy-bastion-81523-f30e287dba50.herokuapp.com/api/proxy'
+    BACKEND_URL: process.env.BACKEND_URL || 'https://sleepy-bastion-81523-f30e287dba50.herokuapp.com/api/proxy'
   };
 }
