fa075a889c0de6cf46efb536fef45ec0d9f055a1
phamduydieu2204 <vidieu.amz@gmail.com>
2025-04-26 16:00:00 +0700

    cập nhật code

--- js/showProcessingModal.js
+++
@@ -1,6 +0,0 @@
-export function showProcessingModal(message) {
-    const modal = document.getElementById("processingModal");
-    const messageElement = document.getElementById("processingMessage");
-    messageElement.textContent = message;
-    modal.style.display = "block";
-  }
